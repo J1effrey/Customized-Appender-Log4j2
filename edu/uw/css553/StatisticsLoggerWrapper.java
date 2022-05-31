0d4f300465c3382ec5e917dceec6bcdb2b441f21
Anirudh Potturi <anirudhp1099@gmail.com>
2022-05-31 12:51:24 -0700

    A custom logger wrapper. Contains basic functionalities. More features to be added.

---
+++ edu/uw/css553/StatisticsLoggerWrapper.java
@@ -0,0 +1,98 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+
+public class StatisticsLoggerWrapper {
+    private final String LoggerName;
+    private Logger logger;
+
+
+    protected StatisticsLoggerWrapper(String loggerName) {
+        LoggerName = loggerName;
+    }
+
+    public String getLoggerName()
+    {
+        return LoggerName;
+    }
+
+    public void debug(Object message)
+    {
+        log.log(Level.DEBUG, message);
+    }
+
+    public void debug(Object message, Throwable t)
+    {
+        log.log(Level.DEBUG, message, t);
+    }
+
+    public boolean isInfoEnabled()
+    {
+        Level p = Level.INFO;
+        if(!log.isEnabledFor(p))
+            return false;
+        else
+            return p.isGreaterOrEqual(log.getEffectiveLevel());
+    }
+
+    public void info(Object message)
+    {
+        log.log(Level.INFO, message);
+    }
+
+    public void info(Object message, Throwable t)
+    {
+        log.log(Level.INFO, message, t);
+    }
+
+    public void warn(Object message)
+    {
+        log.log(Level.WARN, message);
+    }
+
+    public void warn(Object message, Throwable t)
+    {
+        log.log(Level.WARN, message, t);
+    }
+
+    public void error(Object message)
+    {
+        log.log(Level.ERROR, message);
+    }
+
+    public void error(Object message, Throwable t)
+    {
+        log.log(Level.ERROR, message, t);
+    }
+
+    public void fatal(Object message)
+    {
+        log.log(Level.FATAL, message);
+    }
+
+    public void fatal(Object message, Throwable t)
+    {
+        log.log(Level.FATAL, message, t);
+    }
+
+    public void log(Level p, Object message)
+    {
+        log.log(p, message);
+    }
+
+    public void log(Level p, Object message, Throwable t)
+    {
+        log.log(p, message, t);
+    }
+
+    public static StatisticsLoggerWrapper getLogger(String name)
+    {
+        StatisticsLoggerWrapper logger = new StatisticsLoggerWrapper(name);
+        return logger;
+    }
+
+    public static StatisticsLoggerWrapper getLogger(Class fqcn)
+    {
+        StatisticsLoggerWrapper logger = new StatisticsLoggerWrapper(fqcn.getName());
+        return logger;
+    }
+}
